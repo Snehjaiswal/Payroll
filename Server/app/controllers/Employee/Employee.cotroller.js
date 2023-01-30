6aeeb8fb548a92f370fba79cc0c2648f84a04e45
Snehjaiswal <snehj121@gmail.com>
2023-01-30 23:22:58 +0530

    anmol

--- Server/app/controllers/Employee/Employee.cotroller.js
+++ Server/app/controllers/Employee/Employee.cotroller.js
@@ -8,44 +8,41 @@ const { validationResult } = require('express-validator')
 class Employee {
     async AddEmployee(req, res) {
         try {
-            const { FirstName, LastName, PhoneNumber, Email , FatherName , Date_Of_Birth ,  Gender , Local_Address , Martial_Status , Nationality , Profile_Img
+            const { FirstName, LastName, PhoneNumber, Email, FatherName, Date_Of_Birth, Gender, Local_Address, Martial_Status, Nationality, Profile_Img, Deparment,
+                Designation, Date_Of_Joining, Status
             } = req.body;
 
             const errors = validationResult(req)
 
             if (!errors.isEmpty()) {
-                console.log("err=>",errors.array());
-                req.send({msg:errors.array()})
+                console.log("err=>", errors.array());
+                req.send({ msg: errors.array() })
             } else {
 
                 const employ = new EmployeeModal({
-                    FirstName, LastName, PhoneNumber, Email , FatherName , Date_Of_Birth ,  Gender , Local_Address , Martial_Status , Nationality , Profile_Img
+                    FirstName, LastName, PhoneNumber, Email, FatherName, Date_Of_Birth, Gender, Local_Address, Martial_Status, Nationality, Profile_Img
                 });
-    
+
                 //STORE YOUR LOGIN DATA IN DB 
-               const employSave =  await employ.save();
-                console.log( employSave.id );
+                const employSave = await employ.save();
+                // console.log(employSave.id);
 
 
+                const EmployeeComapny = new EmployeeComapnyModal({
+                    Deparment, Designation, Date_Of_Joining, Status,
+                    userid: employSave.id
+
+                });
+                console.log({Deparment, Designation, Date_Of_Joining, Status,
+                    userid: employSave.id});
+                const CompanyInforSave = await EmployeeComapny.save();
 
 
-
-
-
-
-
-
-
-
-                
-                res.send({ msg: "Success ",data:employSave });
+                res.send({ msg: "Success ", data: CompanyInforSave });
             }
-
-
-           
         }
         catch (error) {
-            console.log(error);
+            res.send({ msg: error })
         }
     }
 
