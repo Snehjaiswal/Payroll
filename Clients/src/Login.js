09438ff5d7c070df25a8c3650c2a464856c9acf6
snehpnp <snehpnp@gmail.com>
2023-02-01 17:53:41 +0530

    Accordian try

--- Clients/src/Login.js
+++ Clients/src/Login.js
@@ -1,13 +1,68 @@
 import React from 'react'
+import Accordion from 'react-bootstrap/Accordion';
 
 function Login() {
   return (
     <>
-    
-    <h1>Login</h1>
-    
-    
-    
+
+<Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>
+        <div class="row">
+    <div class="col ">
+      <h6>
+      Idssddddddddddsssssssssssssssssssssssssssss
+      </h6>
+
+    </div>
+    <div class="col ">
+    <h6>
+      UserName
+      </h6>
+    </div>
+    <div class="col ">
+    <h6>
+    Symbol
+      </h6>
+    </div>
+    <div class="col ">
+    <h6>
+      Prize
+      </h6>
+    </div> <div class="col ">
+      One of
+    </div>
+  </div>
+
+          </Accordion.Header>
+        
+
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+
+
     </>
   )
 }
