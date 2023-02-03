ae023a49cfdffb5e3141f442e446d515d4d9ab77
anmol-zd <anmol.r@zecdata.com>
2023-02-03 21:59:04 +0530

    get-emp

--- Server/app/controllers/Employee/Employee.cotroller.js
+++ Server/app/controllers/Employee/Employee.cotroller.js
@@ -6,116 +6,159 @@ const { validationResult } = require('express-validator')
 
 // Product CLASS
 class Employee {
-    async AddEmployee(req, res) {
-        try {
-            const { FirstName, LastName, PhoneNumber, Email, FatherName, Date_Of_Birth, Gender, Local_Address, Martial_Status, Nationality, Profile_Img, Deparment, BankName, Branch_Name, Account_Number, Account_type, IFSC_Code,
-                Designation, Date_Of_Joining, Status, Basic_salary, Increment, IncrementPercent, Bonus
-            } = req.body;
+  async AddEmployee(req, res) {
+    try {
+      const { FirstName, LastName,
+        PhoneNumber, Email,
+        FatherName, Date_Of_Birth,
+        Gender, Local_Address,
+        Martial_Status, Nationality,
+        Profile_Img, Deparment, BankName,
+        Branch_Name, Account_Number,
+        Account_type, IFSC_Code,
+        Designation, Date_Of_Joining,
+        Status, Basic_salary,
+        Increment, IncrementPercent,
+        Bonus
+      } = req.body;
 
-            const errors = validationResult(req)
+      const errors = validationResult(req)
 
-            if (!errors.isEmpty()) {
-                console.log("err=>", errors.array());
-                req.send({ msg: errors.array() })
-            } else {
+      if (!errors.isEmpty()) {
+        console.log("err=>", errors.array());
+        req.send({ msg: errors.array() })
+      } else {
 
-                // Email Validation Check
-                const ExistEmail = await EmployeeModal.findOne({ Email });
+        // Email Validation Check
+        const ExistEmail = await EmployeeModal.findOne({ Email });
 
-                if (ExistEmail)
-                    return res.status(400).json({ msg: "This email already exists." });
-
-                    
-                // Account Number Check 
-                const ExistAccountNumber = await EmployeeAccountModal.findOne({ Account_Number });
-
-                if (ExistAccountNumber)
-                    return res.status(400).json({ msg: "This Bank Account already exists." });
-
-
-
-
-
-                // Personal Account Information
-                const employ = new EmployeeModal({
-                    FirstName, LastName, PhoneNumber, Email, Password: FirstName + "@" + PhoneNumber.slice(-4), FatherName, Date_Of_Birth, Gender, Local_Address, Martial_Status, Nationality, Profile_Img
-                });
-
-                //STORE YOUR LOGIN DATA IN DB 
-                const employSave = await employ.save();
+        if (ExistEmail)
+          return res.status(400).json({ msg: "This email already exists." });
 
 
-                // Company Information
-                const EmployeeComapny = new EmployeeComapnyModal({
-                    Deparment, Designation, Date_Of_Joining, Status,
-                    userid: employSave.id
-                });
-                await EmployeeComapny.save();
+        // Account Number Check 
+        const ExistAccountNumber = await EmployeeAccountModal.findOne({ Account_Number });
 
-                // Bank Account Information
-                const EmployeeAccountInfo = new EmployeeAccountModal({
-                    BankName, Branch_Name, Account_type, Account_Number, IFSC_Code,
-                    userid: employSave.id
-                });
+        if (ExistAccountNumber)
+          return res.status(400).json({ msg: "This Bank Account already exists." });
 
-                await EmployeeAccountInfo.save();
+        // Personal Account Information
+        const employ = new EmployeeModal({
+          FirstName,
+          LastName,
+          PhoneNumber,
+          Email,
+          Password: FirstName + "@" + PhoneNumber.slice(-4),
+          FatherName,
+          Date_Of_Birth,
+          Gender,
+          Local_Address,
+          Martial_Status,
+          Nationality,
+          Profile_Img
+        });
+
+        //STORE YOUR LOGIN DATA IN DB 
+        const employSave = await employ.save();
 
 
-                // Employee Financial Information 
-                const EmployeeFinabcialInfo = new EmployeeFinancialModal({
-                    Basic_salary,Increment, IncrementPercent, Bonus,
-                    userid: employSave.id
-                });
+        // Company Information
+        const EmployeeComapny = new EmployeeComapnyModal({
+          Deparment,
+          Designation,
+          Date_Of_Joining,
+          Status,
+          userid: employSave.id
+        });
+        await EmployeeComapny.save();
 
-                await EmployeeFinabcialInfo.save();
+        // Bank Account Information
+        const EmployeeAccountInfo = new EmployeeAccountModal({
+          BankName,
+          Branch_Name,
+          Account_type,
+          Account_Number,
+          IFSC_Code,
+          userid: employSave.id
+        });
+
+        await EmployeeAccountInfo.save();
+
+
+        // Employee Financial Information 
+        const EmployeeFinabcialInfo = new EmployeeFinancialModal({
+          Basic_salary,
+          Increment,
+          IncrementPercent,
+          Bonus,
+          userid: employSave.id
+        });
+
+        await EmployeeFinabcialInfo.save();
 
 
 
 
-                res.send({ msg: "Success"});
-            }
-        }
-        catch (error) {
-            res.send({ msg: "Error=>" + error })
-        }
+        res.send({ msg: "Success" });
+      }
     }
-
-async aggre(req,res){
-    collection('Payroll')
-    .aggregate(
-      [
-        { $match: { clothId: { '$in': convertInto } } }, // convertInto is arrays of sold clothId which I got from Shopify
-        {
-          $lookup:
-          {
-            from: "bags",
-            localField: "bagId",
-            foreignField: "bagId",
-            as: "bags"
-          }
-        },
-        {
-          $lookup:
-          {
-            from: "sellers",
-            localField: "sellerId",
-            foreignField: "sellerId",
-            as: "sellers"
-          },
-        },
-        {
-          "$project": {
-            "bagId": 1.0,
-            "bags.source": 1.0,
-            "sellers.firstName": 1.0, // dont get anything
-            "sellers.lastName": 1.0,  // dont get anything
-            "brand": 1.0
-          }
-        },
-      ]
-    ).toArray()
-
+    catch (error) {
+      res.send({ msg: "Error=>" + error })
     }
+  }
+  async GetEmployee(req, res, next) {
+    EmployeeAccountModal.find({}).then(function (employee) {
+      res.send(employee);
+    }).catch(next);
+  }
+
+  async aggre(req, res) {
+  //   collection('Payroll')
+  //     .aggregate(
+  //       [
+  //         { $match: { clothId: { '$in': convertInto } } }, // convertInto is arrays of sold clothId which I got from Shopify
+  //         {
+  //           $lookup:
+  //           {
+  //             from: "bags",
+  //             localField: "bagId",
+  //             foreignField: "bagId",
+  //             as: "bags"
+  //           }
+  //         },
+  //         {
+  //           $lookup:
+  //           {
+  //             from: "sellers",
+  //             localField: "sellerId",
+  //             foreignField: "sellerId",
+  //             as: "sellers"
+  //           },
+  //         },
+  //         {
+  //           "$project": {
+  //             "bagId": 1.0,
+  //             "bags.source": 1.0,
+  //             "sellers.firstName": 1.0, // dont get anything
+  //             "sellers.lastName": 1.0,  // dont get anything
+  //             "brand": 1.0
+  //           }
+  //         },
+  //         {
+  //           $lookup: {
+  //             from: "movies",
+  //             localField: "movie_id",
+  //             foreignField: "_id",
+  //             as: "movie_details",
+  //           },
+  //         },
+  //         {
+  //           $limit: 1
+  //         }
+  //       ]
+  //     ).toArray()
+
+  }
 
 
 }
--- Server/app/routes/Employee/Employee.route.js
+++ Server/app/routes/Employee/Employee.route.js
@@ -2,10 +2,10 @@
 "use strict"
 
 const router = require("express").Router()
-const { AddEmployee}= require('../../controllers/Employee/Employee.cotroller')
+const { AddEmployee, aggre, GetEmployee } = require('../../controllers/Employee/Employee.cotroller')
 const { check } = require('express-validator');
 
-router.post('/add',[check('Email').isEmail().normalizeEmail()],AddEmployee)
-
+router.post('/add', [check('Email').isEmail().normalizeEmail()], AddEmployee)
+router.get('/get-all', GetEmployee)
 module.exports = router;
 
