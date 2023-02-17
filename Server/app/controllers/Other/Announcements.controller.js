bcae1c1c97662de974d11fe9949eafd6ae148c63
anmol-zd <anmol.r@zecdata.com>
2023-02-17 14:58:19 +0530

    try-catch

--- Server/app/controllers/Other/Announcements.controller.js
+++ Server/app/controllers/Other/Announcements.controller.js
@@ -1,22 +1,26 @@
-const {AnnouncementsModel} = require('../../Models/Others/Announcements.model')
+const { AnnouncementsModel } = require('../../Models/Others/Announcements.model')
 
 
 class Announcements {
 
     async AddAnnouncements(req, res) {
         // console.log("req.",req.body)
+        try {
+            const { title, from_date, end_date, msg } = req.body
 
-        const { title,from_date,end_date,msg} = req.body
+            const AnnouncementsData = new AnnouncementsModel({
+                title, from_date, end_date, msg
+            });
 
-        const AnnouncementsData = new AnnouncementsModel({
-            title,from_date,end_date,msg
-        });
+            //STORE YOUR LOGIN DATA IN DB 
+            const AnnouncementsSave = await AnnouncementsData.save();
+            // console.log("HolidaysSave",HolidaysSave)
 
-        //STORE YOUR LOGIN DATA IN DB 
-        const  AnnouncementsSave = await AnnouncementsData.save();
-        // console.log("HolidaysSave",HolidaysSave)
-        
-        res.send({ msg: "Success"});
+            res.send({ msg: "Success" });
+        }
+        catch (error) {
+            res.send({ msg: "error", error });
+        }
     }
 
 
@@ -26,15 +30,15 @@ class Announcements {
         // console.log("req.",res)
 
         const AnnouncementsData = await AnnouncementsModel.find();
-        console.log("fff",AnnouncementsData);
-        if(AnnouncementsData){
-            res.send({Announcements:AnnouncementsData})
-        }else{
-            res.send({msg:"Error"})
+        console.log("fff", AnnouncementsData);
+        if (AnnouncementsData) {
+            res.send({ Announcements: AnnouncementsData })
+        } else {
+            res.send({ msg: "Error" })
 
 
         }
-      
+
     }
 
 }
